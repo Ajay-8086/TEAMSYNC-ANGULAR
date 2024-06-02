bc864919ea4b01eeceee78fd63be40f7be8757d3
Ajay <tajay5767@gmail.com>
2024-06-02 20:08:37 +0530

    login page created

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,18 +2,20 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RegisterComponent } from "./components/register/register.component";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/Login/login.component";
 
 
 
 const routes:Routes = [
     {path:'register',component:RegisterComponent},
+    {path:'login',component:LoginComponent},
     
 ]
 
 @NgModule({
     imports:[CommonModule,RouterModule.forChild(routes)],
     providers:[],
-    declarations:[RegisterComponent]
+    declarations:[RegisterComponent,LoginComponent]
 })
 export class AuthModule{
 
