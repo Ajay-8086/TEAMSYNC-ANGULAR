65eb32d9ec961170600548d454fb5f5c29b63ae1
Ajay <tajay5767@gmail.com>
2024-06-03 00:31:48 +0530

    auth component updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/components/home.component';
 
 const routes: Routes = [
+  // default route
   {path:'',component:HomeComponent},
+  // lazy loaded  module register and login
   {path:'auth',loadChildren:()=>import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from "@angular/core";
 import { RegisterComponent } from "./components/register/register.component";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./components/Login/login.component";
+import { AuthComponent } from "./auth.component";
 
 
 
 const routes:Routes = [
-    {path:'register',component:RegisterComponent},
-    {path:'login',component:LoginComponent},
+    {path:'',component:AuthComponent,children:[ {path:'register',component:RegisterComponent},
+    {path:'login',component:LoginComponent},]}
+   
     
 ]
 
 @NgModule({
     imports:[CommonModule,RouterModule.forChild(routes)],
     providers:[],
-    declarations:[RegisterComponent,LoginComponent]
+    declarations:[RegisterComponent,LoginComponent,AuthComponent]
 })
 export class AuthModule{
 
